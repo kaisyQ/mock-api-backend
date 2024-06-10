2ba941bd150eacfe2923d4952b3a7efcf0cf09f5
kaisyQ <rutyo8@mail.ru>
2024-06-10 16:15:22 +1000

    fix tests&package version

--- src/test/java/com/example/mocks/MocksApplicationTests.java
+++ src/test/java/com/example/mocks/MocksApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.mocks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootTest(classes = MocksApplication.class)
+class MocksApplicationTests {
 
 	@Test
 	void contextLoads() {
